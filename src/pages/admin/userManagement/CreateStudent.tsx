0f022909e0016f5bad5c11af14ddd5582eebe429
mstsurnalyakter <surnalyms@gmail.com>
2025-09-01 15:12:34 +0600

    refactor: Remove hardcoded student data and default values from CreateStudent component

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -21,75 +21,7 @@ import {
 import type { TAcademicDepartment, TAcademicSemester } from '../../../types';
 import { useAddStudentMutation } from '../../../redux/features/admin/userManagement.api';
 
-const studentData = {
-  password: 'student123',
-  student: {
-    name: {
-      firstName: 'I am ',
-      middleName: 'Student',
-      lastName: 'Number 1',
-    },
-    gender: 'male',
-    dateOfBirth: '1990-01-01',
-    email: 'student2@gmail.com',
-    contactNo: '1235678',
-    emergencyContactNo: '987-654-3210',
-    bloogGroup: 'A+',
-    presentAddress: '123 Main St, Cityville',
-    permanentAddress: '456 Oak St, Townsville',
-    guardian: {
-      fatherName: 'James Doe',
-      fatherOccupation: 'Engineer',
-      fatherContactNo: '111-222-3333',
-      motherName: 'Mary Doe',
-      motherOccupation: 'Teacher',
-      motherContactNo: '444-555-6666',
-    },
-    localGuardian: {
-      name: 'Alice Johnson',
-      occupation: 'Doctor',
-      contactNo: '777-888-9999',
-      address: '789 Pine St, Villageton',
-    },
-    admissionSemester: '65b0104110b74fcbd7a25d92',
-    academicDepartment: '65b00fb010b74fcbd7a25d8e',
-  },
-};
 
-const studentDefaultValues = {
-  name: {
-    firstName: 'I am ',
-    middleName: 'Student',
-    lastName: 'Number 1',
-  },
-  gender: 'male',
-
-  bloogGroup: 'A+',
-
-  contactNo: '1235678',
-  emergencyContactNo: '987-654-3210',
-  presentAddress: '123 Main St, Cityville',
-  permanentAddress: '456 Oak St, Townsville',
-
-  guardian: {
-    fatherName: 'James Doe',
-    fatherOccupation: 'Engineer',
-    fatherContactNo: '111-222-3333',
-    motherName: 'Mary Doe',
-    motherOccupation: 'Teacher',
-    motherContactNo: '444-555-6666',
-  },
-
-  localGuardian: {
-    name: 'Alice Johnson',
-    occupation: 'Doctor',
-    contactNo: '777-888-9999',
-    address: '789 Pine St, Villageton',
-  },
-
-  // admissionSemester: '65bb60ebf71fdd1add63b1c0',
-  // academicDepartment: '65b4acae3dc8d4f3ad83e416',
-};
 
 const CreateStudent = () => {
   const { data: sData, isLoading: sIsLoading } =
